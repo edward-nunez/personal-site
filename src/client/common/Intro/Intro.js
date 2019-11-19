4db21446342029fb122bcb42df8fa0f7cbe5c4b2
dotRollen <edwardnnz@gmail.com>
2019-11-18 23:10:39 -0500

    [╭( ･ㅂ･)و ] Added intro component

---
+++ src/client/common/Intro/Intro.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Intro(props) {
+  return (
+    <div className="intro" style={{ paddingTop: '40px' }}>
+      <h2>Home</h2>
+    </div>
+  );
+}
