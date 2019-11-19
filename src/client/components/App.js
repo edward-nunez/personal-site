1f39968d02f4d30497369314c8c3fd0062ed3a70
dotRollen <edwardnnz@gmail.com>
2019-11-18 23:11:31 -0500

    [ _〆(･_･｡) ] Changed application component to include new components

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
 
 import Routes from '../routes';
 import Navigation from 'Common/Navigation';
+import Banner from 'Common/Banner';
+import Footer from 'Common/Footer';
 import 'Styles/App.scss';
 
 function App() {
   return (
     <React.Fragment>
-      <Navigation />
-      <section>
+      <header className="header">
+        <Navigation path={window.location.pathname} />
+        <div className="container">
+          <Banner />
+        </div>
+      </header>
+      <section className="content">
         <Routes />
       </section>
+      <Footer />
     </React.Fragment>
   );
 }
-
-App.propTypes = {
-  history: PropTypes.object,
-};
 
 export default hot(module)(App);
