acc62d13ae5542da74d88ec0aea8fa0f9587ac0d
dotRollen <edwardnnz@gmail.com>
2019-11-21 21:19:54 -0500

    [ _〆(･_･｡) ] Changed general styling of the banner

--- src/client/common/Banner/Banner.js
+++ src/client/common/Banner/Banner.js
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import { connect } from 'react-redux';
 
-import 'Styles/Banner.scss';
+import './Banner.style.scss';
+import Logo from '../Logo';
 
 function Banner(props) {
   const setBanner = () => {
@@ -10,32 +12,29 @@ function Banner(props) {
     switch (props.pathname) {
       case '/':
         obj = {
-          title: 'Full Stack Engineer',
+          img: '/assets/img/template/logo.svg',
           body:
-            'Hello and welcome! I appreciate you taking the time to view my portfolio. ',
+            'Hello and welcome! I appreciate you taking the time \
+            to view my portfolio.',
         };
         break;
       case '/projects':
         obj = {
-          title: 'Projects',
           body: 'projects body',
         };
         break;
       case '/blog':
         obj = {
-          title: 'Blog',
           body: 'blog body',
         };
         break;
       case '/about':
         obj = {
-          title: 'About',
           body: 'about body',
         };
         break;
       case '/contact':
         obj = {
-          title: 'contact test',
           body: 'contact body',
         };
         break;
@@ -50,18 +49,13 @@ function Banner(props) {
   const banner = setBanner();
 
   return (
-    <div
-      className="container banner"
-      hidden={!banner}
-      style={{ height: '400px', paddingTop: '100px' }}
-    >
+    <div className="container banner" hidden={!banner}>
       <div className="row">
-        <div className="col-md-4" hidden={!banner.img}>
-          <h1>image placeholder</h1>
+        <div className="col-12" hidden={!banner.img}>
+          <Logo />
         </div>
-        <div className="col-lg-8 col-md-8">
-          <h1 className="banner-title">{banner.title}</h1>
-          <p className="banner-body">{banner.body}</p>
+        <div className="col-12">
+          <p>{banner.body}</p>
         </div>
       </div>
     </div>
