144a068e63a3e7387328fefabc0cd96d4a8b216b
dotRollen <edwardnnz@gmail.com>
2019-11-21 21:17:52 -0500

    [（╬ಠ益ಠ) ] Removed intro component

--- src/client/common/Intro/Intro.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Intro(props) {
-  return (
-    <div className="intro" style={{ paddingTop: '40px' }}>
-      <h2>Home</h2>
-    </div>
-  );
-}
