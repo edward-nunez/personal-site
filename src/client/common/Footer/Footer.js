84e6582686f01eb471c632fb51a1ebaffd2c9cf0
dotRollen <edwardnnz@gmail.com>
2019-11-21 21:19:40 -0500

    [ _〆(･_･｡) ] Changed general styling of footer

--- src/client/common/Footer/Footer.js
+++ src/client/common/Footer/Footer.js
@@ -2,116 +2,106 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faTwitterSquare,
-  faGithubSquare,
-  faDribbbleSquare,
-  faMedium,
+  faTwitter,
+  faGithubAlt,
+  faDribbble,
+  faMediumM,
   faInstagram,
-  faLinkedin,
+  faLinkedinIn,
 } from '@fortawesome/free-brands-svg-icons';
 
-import 'Styles/Footer.scss';
+import './Footer.style.scss';
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="container row" style={{ paddingTop: '40px' }}>
-        <div className="col-sm-12 col-md-6 row">
-          <div className="col-12">
-            <h1>Edward Nunez</h1>
-            <p>
-              “When you don’t have resources, you become resourceful.” - KR
-              Sridhar
-            </p>
-          </div>
-        </div>
-        <div className="col-sm-12 col-md-6 row">
-          <div className="col-12" style={{ marginBottom: '10px' }}>
-            Where you can find me:
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://github.com/dotRollen"
-              target="_blank"
+    <footer className="footer fixed-bottom">
+      <div className="container">
+        <div className="row" style={{ paddingTop: '40px' }}>
+          <div className="col-12 row">
+            <div
+              className="col-12"
+              style={{ marginBottom: '10px', fontSize: '18px' }}
             >
-              <FontAwesomeIcon
-                icon={faGithubSquare}
-                size="2x"
-                className="social-icon"
-              />
-              GitHub
-            </a>
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://twitter.com/edwardnnz"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                icon={faTwitterSquare}
-                size="2x"
-                className="social-icon"
-              />
-              Twitter
-            </a>
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://www.instagram.com/dotrollen/"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                icon={faInstagram}
-                size="2x"
-                className="social-icon"
-              />
-              Instagram
-            </a>
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://medium.com/@edwardnnz"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                icon={faMedium}
-                size="2x"
-                className="social-icon"
-              />
-              Medium
-            </a>
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://www.linkedin.com/in/edwardnunez/"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                size="2x"
-                className="social-icon"
-              />
-              LinkedIn
-            </a>
-          </div>
-          <div className="col-6" style={{ marginBottom: '10px' }}>
-            <a
-              className="social-link"
-              href="https://dribbble.com/dotRollen"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                icon={faDribbbleSquare}
-                size="2x"
-                className="social-icon"
-              />
-              Dribbble
-            </a>
+              Where you can find me:
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://github.com/dotRollen"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faGithubAlt}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://twitter.com/edwardnnz"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://www.instagram.com/dotrollen/"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://medium.com/@edwardnnz"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faMediumM}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://www.linkedin.com/in/edwardnunez/"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedinIn}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
+            <div className="col-6" style={{ marginBottom: '10px' }}>
+              <a
+                className="social-link"
+                href="https://dribbble.com/dotRollen"
+                target="_blank"
+              >
+                <FontAwesomeIcon
+                  icon={faDribbble}
+                  size="2x"
+                  className="social-icon"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
