ac2de664c2a66a2a6a37f810595d89c4220da740
dotRollen <edwardnnz@gmail.com>
2019-12-28 03:51:59 -0500

    [╭( ･ㅂ･)و ] Added general structure of blog list view

--- src/client/components/Blog/Blog.js
+++ src/client/components/Blog/Blog.js
@@ -1,15 +1,50 @@
 import React from 'react';
 
-import BlogPost from './BlogPost';
+import BlogCard from '../../common/BlogCard/BlogCard';
+
+import '../App.style.scss';
 
 export default function Blog() {
   return (
-    <div className="container">
+    <div className="container content">
       <div className="row">
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
-        <BlogPost />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+        <nav
+          className="col-12"
+          style={{ marginLeft: '20px' }}
+          aria-label="Page navigation example"
+        >
+          <ul className="pagination">
+            <li className="page-item">
+              <a className="page-link" href="#">
+                Previous
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
