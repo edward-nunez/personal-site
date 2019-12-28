abe93933b9bd374f0a6943e9770d71b0923430ca
dotRollen <edwardnnz@gmail.com>
2019-12-28 03:46:40 -0500

    [╭( ･ㅂ･)و ] Added transition animation between views

--- src/client/components/About/About.js
+++ src/client/components/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import '../App.style.scss';
+
 export default function About() {
   return (
-    <div className="container">
+    <div className="container content">
       <h1>About</h1>
     </div>
   );
--- src/client/components/Projects/Projects.js
+++ src/client/components/Projects/Projects.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import '../App.style.scss';
+
 export default function Projects() {
   return (
-    <div className="container">
+    <div className="container content">
       <h1>PROJECTS</h1>
     </div>
   );
