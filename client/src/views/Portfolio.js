c4baa5cb78e9b7009338ec279fe534991223b8dd
edward.nunez <edwardnnz@gmail.com>
2022-07-17 22:24:44 -0400

    added api call and simplified view code

--- client/src/views/Portfolio.js
+++ client/src/views/Portfolio.js
@@ -1,11 +1,23 @@
 import React from "react";
+import useAxios from "axios-hooks";
 
-import Projects from "../components/sections/Projects";
+import ProjectCard from "../components/elements/ProjectCard";
 
 export default function Portfolio() {
+  const [{ data, loading, error }, refetch] = useAxios(
+    "http://localhost:3001/api/projects"
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error!</p>;
+
   return (
     <div className="row">
-      <Projects />
+      <div className="col-md-12 mx-auto">
+        {data.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </div>
   );
 }
