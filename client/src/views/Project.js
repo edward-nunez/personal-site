97f03d2d1af545f6b65198f0dd45ea9b29683891
edward.nunez <edwardnnz@gmail.com>
2022-07-17 22:25:22 -0400

    updated project page for api data

--- client/src/views/Project.js
+++ client/src/views/Project.js
@@ -1,42 +1,60 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-import projectsData from "../utils/projectsData.json";
-
-export default function Project(props) {
+export default function Project() {
+  const location = useLocation();
+  const { projectData } = location.state;
 
   return (
     <div className="container content">
       <article className="row" style={{ margin: "20px 0px" }}>
         <div className="col-12">
-          <h2>{projectsData[0].title}</h2>
+          <h2>{projectData.title}</h2>
         </div>
         <div className="col-12">
-          <a href={projectsData[0].liveSite}><img src={projectsData[0].titleImage} 
-          className="img-fluid rounded-start" alt="..."/></a>
+          <a href={projectData.liveSite}>
+            <img
+              src={projectData.titleImage}
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </a>
         </div>
         <div className="col-10">
-          <h3 className="text-muted">{projectsData[0].description}</h3>
+          <h3 className="text-muted">{projectData.description}</h3>
         </div>
         <div className="col-2">
-          <a href={projectsData[0].liveSite} className="btn btn-primary position-relative">
-            Live Site <span className="position-absolute top-0 start-100 translate-middle badge 
-            rounded-pill bg-danger"> !
+          <a
+            href={projectData.liveSite}
+            className="btn btn-primary position-relative"
+          >
+            Live Site{" "}
+            <span
+              className="position-absolute top-0 start-100 translate-middle badge 
+            rounded-pill bg-danger"
+            >
+              {" "}
+              !
             </span>
           </a>
         </div>
         <div className="col-12">
-          <p><small className="text-muted">Tags </small>{projectsData[0].tags.map((tag, index) => (
-            <span key={index} className="badge bg-light text-dark">{tag}</span>)
-          )}
+          <p>
+            <small className="text-muted">Tags </small>
+            {projectData.tags.map((tag, index) => (
+              <span key={index} className="badge bg-light text-dark">
+                {tag.name}
+              </span>
+            ))}
           </p>
         </div>
         <div className="col-12">
           <h2 className="text-muted">Criteria</h2>
-          <p>{projectsData[0].criteria}</p>
+          <p>{projectData.criteria}</p>
         </div>
         <div className="col-12">
           <h2 className="text-muted">Results</h2>
-          <p>{projectsData[0].results}</p>
+          <p>{projectData.results}</p>
         </div>
       </article>
     </div>
