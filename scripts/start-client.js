35ddacfc111a13b5652ae535fd1cfe386387eb17
edward.nunez <edwardnnz@gmail.com>
2022-11-09 20:23:13 -0500

    simplified project structure

--- scripts/start-client.js
+++
@@ -1,6 +0,0 @@
-// Script for changing workspace to client directory. Makes it so client/package.json is root
-// and not ./package.json
-const args = ['start'];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-
-require('child_process').spawn('yarn', args, opts);
