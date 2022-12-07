d024a188bfc1af79c7aec477088fb73059318066
edward.nunez <edwardnnz@gmail.com>
2022-12-06 21:14:19 -0500

    Updated all react dependencies and render method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import * as serviceWorker from './serviceWorker';
@@ -11,12 +11,15 @@ import 'bootstrap/dist/js/bootstrap.min';
 import App from './App';
 
 const history = createBrowserHistory();
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-  <Router history={history}>
-    <App />
-  </Router>,
-  document.getElementById('root')
+root.render(
+  // https://reactjs.org/docs/strict-mode.html
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
