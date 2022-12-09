60a555052e033425101c6591acec3a3c42c33202
edward.nunez <edwardnnz@gmail.com>
2022-12-08 21:48:43 -0500

    Added web vitals, updated index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import * as serviceWorker from './serviceWorker';
-
-import './assets/scss/style.scss';
 import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
@@ -20,7 +20,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
