e9feb7f175e21df7aa5c14c72fc837db37e88194
edward.nunez <edwardnnz@gmail.com>
2022-12-11 12:29:30 -0500

    Updated variables to be more self explanatory

--- src/components/elements/ReCaptcha.js
+++ src/components/elements/ReCaptcha.js
@@ -2,25 +2,20 @@ import PropTypes from 'prop-types';
 
 import ReCAPTCHA from 'react-google-recaptcha';
 
-/**
- * Simple ReCAPTCHA to prevent bots and spammers from abusing functionality.
- *
- * @param TODO
- */
-function ReCaptcha({ updateValid }) {
-  const handleValidChange = (valid) => {
+function ReCaptcha({ updateReCaptchaState }) {
+  const handleOnChange = (valid) => {
     if (valid !== null) {
-      updateValid(true); // True if value has valid captcha response stored
+      updateReCaptchaState(true); // True if value has valid captcha response stored
     } else {
-      updateValid(false); // False if value is null for no valid captcha response
+      updateReCaptchaState(false); // False if value is null for no valid captcha response
     }
   };
 
-  return <ReCAPTCHA sitekey="6LeFmQkTAAAAAIwpph9vOJU7yCZQ2mAWb25B-VET" onChange={handleValidChange} />;
+  return <ReCAPTCHA sitekey="6LeFmQkTAAAAAIwpph9vOJU7yCZQ2mAWb25B-VET" onChange={handleOnChange} />;
 }
 
 ReCaptcha.propTypes = {
-  updateValid: PropTypes.func.isRequired,
+  updateReCaptchaState: PropTypes.func.isRequired,
 };
 
 export default ReCaptcha;
